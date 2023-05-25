7675840d0649c4aa2a7fa6364910729e4938db84
ThomasVC <125548342+ThomasVC123@users.noreply.github.com>
2023-05-25 10:57:28 +0200

    Add files via upload
    updated code

--- OOP_Project/Page4/js/script.js
+++ OOP_Project/Page4/js/script.js
@@ -1,13 +1,26 @@
 function renderTasks() {
-    const tasksContainer = document.getElementById('tasks');
-    let tasks = [];
-    if (localStorage.getItem('tasks')) {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
+  const tasksContainer = document.getElementById('tasks');
+  let tasks = [];
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasksContainer.innerHTML = '';
   
-    tasksContainer.innerHTML = '';
+  if (tasks.length > 0) {
+    const table = document.createElement('table');
+    const headerRow = document.createElement('tr');
+    const checkboxHeader = document.createElement('th');
+    checkboxHeader.innerHTML = 'Done';
+    const descriptionHeader = document.createElement('th');
+    descriptionHeader.innerHTML = 'Description';
+    headerRow.appendChild(checkboxHeader);
+    headerRow.appendChild(descriptionHeader);
+    table.appendChild(headerRow);
+
     tasks.forEach(task => {
-      const taskElement = document.createElement('div');
+      const row = document.createElement('tr');
+      const checkboxCell = document.createElement('td');
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.checked = task.done;
@@ -15,17 +28,21 @@ function renderTasks() {
         task.done = checkbox.checked;
         localStorage.setItem('tasks', JSON.stringify(tasks));
       });
-  
-      const label = document.createElement('label');
-      label.innerHTML = task.description;
-  
-      taskElement.appendChild(checkbox);
-      taskElement.appendChild(label);
-      tasksContainer.appendChild(taskElement);
+      checkboxCell.appendChild(checkbox);
+      row.appendChild(checkboxCell);
+
+      const descriptionCell = document.createElement('td');
+      descriptionCell.innerHTML = task.description;
+      row.appendChild(descriptionCell);
+
+      table.appendChild(row);
     });
+
+    tasksContainer.appendChild(table);
+  } else {
+    tasksContainer.innerHTML = 'No tasks found.';
   }
-  function goToLoginScreen() {
-    window.location.href = 'index.html';
-  }
-  
-  
+}
+function goToLoginScreen() {
+  window.location.href = 'index.html';
+}
